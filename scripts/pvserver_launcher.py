6b795e7b5657048704dd5d3f76cd7debf399f306
Jamil Appa <jamil.appa@zenotech.com>
2016-04-11 18:42:08 +0100

    Added ability to prepend shell command and user defined mpiexec

--- scripts/pvserver_launcher.py
+++ scripts/pvserver_launcher.py
@@ -6,13 +6,28 @@ from pvconnect import pvserver_process
 if len(sys.argv) < 5:
     print 'Incorrect usage'
     print ('pververlauncher.py server_port user@host' +
-           ' remote_paraview_location mpi_num_tasks')
+           ' remote_paraview_location mpi_num_tasks' +
+           ' mpiexec (optional)' +
+           ' shell prefix command (optional)')
     sys.exit(0)
 
 data_host = sys.argv[2]
 data_dir = '.'
+remote_location = sys.argv[3]
 job_ntasks = sys.argv[4]
-paraview_cmd = 'mpiexec -n ' + str(job_ntasks) + ' ' + sys.argv[3]+'/pvserver'
+mpiexec = 'mpiexec'
+if len(sys.argv) > 5:
+    mpiexec = sys.argv[5]
+shell_cmd = ''
+if len(sys.argv) > 6:
+    for i in range(6, len(sys.argv)):
+        shell_cmd += sys.argv[i] + ' '
+paraview_cmd = (shell_cmd +
+                mpiexec + ' -n ' +
+                str(job_ntasks) +
+                ' ' +
+                remote_location +
+                '/pvserver')
 
 pvserver_process(data_host=data_host,
                  data_dir=data_dir,
