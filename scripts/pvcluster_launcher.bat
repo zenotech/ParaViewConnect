66e0772235f75a56238103dd050d973d961106f8
Jamil Appa <jappa@users.noreply.github.com>
2021-02-24 15:57:04 +0000

    Remove paraview path

--- scripts/pvcluster_launcher.bat
+++ scripts/pvcluster_launcher.bat
@@ -3,12 +3,6 @@ pushd "%~dp0"
 echo "Activating virtual environment"
 call ..\pvconnect-py27\scripts\activate
 
-call ..\pvconnect-py27\pv-location.bat
-
-echo "%PARAVIEW_BIN_LOCATION%"
-set PATH=%PARAVIEW_BIN_LOCATION%;%PATH%
-set PYTHONPATH=..;%PARAVIEW_SITE_LIB%\site-packages;%PARAVIEW_SITE_LIB%\site-packages\vtk;%PYTHONPATH%
-
 echo %PYTHONPATH%
 
 python pvcluster_launcher.py %* 
