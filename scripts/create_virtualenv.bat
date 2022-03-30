35bdea50723dfc8e9fcf5643a4e63eda6163c933
Jamil Appa <jamil.appa@zenotech.com>
2022-03-30 10:44:11 +0100

    Fix for windows python3

--- scripts/create_virtualenv.bat
+++ scripts/create_virtualenv.bat
@@ -2,20 +2,11 @@
 
 echo "ParaViewConnect installer"
 
-echo "Checking for virtualenv"
-
-REM Check for virtualenv
-WHERE virtualenv --version 2>NUL
-IF %ERRORLEVEL% == 0 GOTO VirtualEnvOK
-ECHO virtualenv not found
-GOTO EOF
-
-:VirtualEnvOK
 
 if exist "./pvconnect-py27/" rd /q /s "./pvconnect-py27/"
 
 echo "Creating virtual environment"
-virtualenv pvconnect-py27
+py -m venv pvconnect-py27
 IF %ERRORLEVEL% == 0 GOTO VirtualEnvCreated
 ECHO virtualenv could not be created
 GOTO EOF
@@ -25,13 +16,8 @@ GOTO EOF
 echo "Activating virtual environment"
 call pvconnect-py27\scripts\activate
 
-echo "Installing yolk"
-pip install yolk
-
 echo "Installing requirements"
 pip install -r requirements.txt 
-
-yolk -l
 
 deactivate
 
